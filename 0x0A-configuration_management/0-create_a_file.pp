2467a0b2919de9c85fb14dedf3b320919e9734c4
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-01-19 10:45:32 -0800

    config

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# using puppet to create a file in /tmp
+
+file { '0-create_a_file':
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
