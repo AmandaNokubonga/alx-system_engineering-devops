9652e8c80de07443325a279ab6dbb2d4951f414a
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-03-14 01:09:24 -0700

    pppp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,12 @@
+# Debug increase limit files per user
 exec { 'change-os-configuration-for-holberton-user':
   environment => ['DIR=/etc/security/limits.conf',
                   'OLD=hard nofile 5',
                   'NEW=hard nofile 50000',
                   'OLD2=soft nofile 4',
                   'NEW2=soft nofile 40000'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR && sudo sed -i "s/$OLD2/$NEW2/" $DIR',
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo sed -i "s/$OLD2/$NEW2/" $DIR',
   path        => ['/usr/bin', '/bin'],
-  provider    => shell,
-  onlyif      => "grep -qF '$NEW' $DIR && grep -qF '$NEW2' $DIR",
+  returns     => [0, 1]
 }
+© 2020 GitHub, Inc.i
