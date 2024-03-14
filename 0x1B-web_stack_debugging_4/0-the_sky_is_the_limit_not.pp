229315048f1c503780fe4df8ce2af6ceda7384e7
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-03-14 01:03:25 -0700

    pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,9 @@
+# Debug web open files per user
+exec { 'fix--for-nginx':
+  environment => ['DIR=/etc/default/nginx',
+                  'OLD=ULIMIT="-n 15"',
+                  'NEW=ULIMIT="-n 15000"'],
+  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR; sudo service nginx restart',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
+}i
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+this project is called 0x1B-web_stack_debugging_4 it is done my AmandaNokubonga
